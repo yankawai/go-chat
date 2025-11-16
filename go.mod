87f9b2e8a91b2ac053950d08c97a7eda0feec21d
yan <yanshinjuku@gmail.com>
2025-11-17 00:07:04 +0300

    add websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mod
 
 go 1.24.3
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func ws(w http.ResponseWriter, r *http.Request)  {
+	c,err := upgrader.Upgrade(w,r,nil)
+	if err != nil { 
+		return
+	}
+	defer c.Close()
+	for { 
+		mt,msg,err  := c.ReadMessage()
+		if err != nil {break}
+		if  err = c.WriteMessage(mt,msg);err != nil{break}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws",ws)
+	http.Handle("/",http.FileServer(http.Dir(".")))
+	fmt.Println("server hosted on: http://localhost:8080/ws")
+	err := http.ListenAndServe(":8080",nil) 	
+	if err != nil { 
+		panic(err)
+	}
 }
